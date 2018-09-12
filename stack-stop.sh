315cd2a9bfabffbb3982bda4daf903d2953b8b0c
root <root@7392A57>
2018-09-12 15:35:04 +0000

    Add stack stop script

--- stack-stop.sh
+++ stack-stop.sh
@@ -5,5 +5,15 @@
 # Is based in source: https://github.com/evertramos/docker-compose-letsencrypt-nginx-proxy-companion
 #
 
-# 1. Stop stack
+# 1. Check if .env file exists
+if [ -e .env ]; then
+    source .env
+else 
+    echo "Please set up your .env file before starting your environment."
+    exit 1
+fi
+
+# 2. Stop stack
 docker stack rm $STACK_NAME
+
+exit 0
